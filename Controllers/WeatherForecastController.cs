f03602e9c42d97ce57ceb14570f66b779103097c
Hubert Wójtowicz <hubert.wojtowicz@geotik.com>
2020-11-20 10:39:57 +0100

    implemented canceling capability example

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CancelingRequestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{delayMilliseconds:int}")]
+        public async Task<IEnumerable<WeatherForecast>> Get(CancellationToken cancellationToken, int delayMilliseconds)
+        {
+            _logger.LogInformation("Request started.");
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            await Task.Delay(delayMilliseconds, cancellationToken);
+            sw.Stop();
+            _logger.LogInformation($"Canceled after {sw.ElapsedMilliseconds}ms.");
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
